629364be111d74f5ce57a0df7a1a280650292627
Nick <nicolasmz0511@gmail.com>
2024-05-06 00:10:47 +0200

    añadido cambio de partituras

--- app.py
+++ app.py
@@ -381,6 +381,7 @@ def mensajeAyuda():
     print("| o -> Mostrar panel de configuración de parámetros de detección de círculos    |")
     print("| i -> Reiciciar progreso en partitura                                          |")
     print("| Espacio -> Recapturar referencias de posiciones de los agujeros de la flauta  |")
+    print("| 1, 2 o 3 -> Seleccionar partitura                                             |")
     print("---------------------------------------------------------------------------------\n\n")
 
     
@@ -406,7 +407,10 @@ def main():
     center = int(rect_width / 2)
 
     posNotaActual = 0
-    partitura = ["Re", "Re", "La", "La", "Si", "Si", "La#"]
+    partitura = []
+    partitura1 = ["Re", "La", "La", "Re", "Sol", "Fa", "La#", "Mi", "Si"]
+    partitura2 = ["Do", "Mi", "Re", "Sol", "Sol#", "Mi", "Si", "Fa#"]
+    partitura3 = ["Sol", "Sol", "Fa", "Re", "Do", "La", "Mi", "Re"]
     
     mensajeAyuda()
 
@@ -422,17 +426,13 @@ def main():
             drawPartitura(frame, partitura)
             printNota(partitura, frame)
 
-        # Dibujar el rectángulo en el frame
         if(showRectangulo):
             cv2.rectangle(frame, (rect_x, rect_y), (rect_x + rect_width, rect_y + rect_height), (255, 0, 255), 3)
 
-        # Obtener la región de interés (ROI) dentro del rectángulo
         roi = frame[rect_y:rect_y+rect_height, rect_x:rect_x+rect_width]
 
-        # Convertir la ROI a escala de grises
         gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
-        # Aplica un desenfoque gaussiano para reducir el ruido
         blurred = cv2.GaussianBlur(gray, (9, 9), 2)
 
         circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, gray.shape[0] / 16, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
@@ -456,7 +456,7 @@ def main():
             instanteInicial = time.monotonic()
 
 
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #_deteccion_manos
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         results = manos.process(frame_rgb)
 
@@ -473,7 +473,7 @@ def main():
                         x = int(landmark.x * frame.shape[1])
                         y = int(landmark.y * frame.shape[0])
 
-                        if idx == 8 or idx == 12 or idx == 16 or idx == 20 :  # Índice, corazón, anular, meñique
+                        if idx == 8 or idx == 12 or idx == 16 or idx == 20 :  # Dedos índice, corazón, anular, meñique
                             if(showLandmarks):
                                 cv2.circle(frame, (x, y), 5, (255, 0, 0), -1) 
 
@@ -537,7 +537,15 @@ def main():
         if cv2.waitKey(5) & 0xFF == ord('i'):
             posNotaActual = 0
             notaTocada = -1
+        
+        if cv2.waitKey(5) & 0xFF == ord('1'):
+            partitura = partitura1
 
+        if cv2.waitKey(5) & 0xFF == ord('2'):
+            partitura = partitura2
+
+        if cv2.waitKey(5) & 0xFF == ord('3'):
+            partitura = partitura3
 
     cap.release()
     cv2.destroyAllWindows()
@@ -545,3 +553,88 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+# Método de detección con Máscara. Escrito aquí por si se tiene curiosidad de la implementación de la máscara a pesar de no usarse
+'''def detectar_circulos(imagen):
+
+    global circle_positions_history
+
+    heightScreen, widthScreen = imagen.shape[:2]
+
+    rect_width = 100
+    rect_height = 400
+    rect_x = int((widthScreen / 2) - (rect_width / 2)) 
+    rect_y = 100
+
+    centro = int(rect_x + (rect_width / 2.0))
+    x, y, w, h = rect_x, rect_y, rect_width, rect_height
+
+    # Dibujar el rectángulo en la imagen
+    cv2.rectangle(imagen, (rect_x, rect_y), (rect_x + rect_width, rect_y + rect_height), (255, 0, 255), 3)
+
+    # Convertir la imagen de BGR a HSV
+    hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
+
+    flute_hue = 30  # Tonos amarillos y dorados
+    hue_tolerance = 26  # Permitir cierta variación en el tono
+    min_saturation = 0  # Saturación mínima
+    max_saturation = 255  # Saturación máxima
+    min_value = 0  # Valor mínimo (brillo)
+    max_value = 255  # Valor máximo (brillo)
+
+    # Calcular los límites inferiores y superiores del rango de color en HSV
+    lower_color = np.array([flute_hue - hue_tolerance, min_saturation, min_value])
+    upper_color = np.array([flute_hue + hue_tolerance, max_saturation, max_value])
+
+    # Crear una máscara que identifique los píxeles dentro del rango de color
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+
+    # Aplicar la máscara para aislar la flauta en la imagen original
+    flute_only = cv2.bitwise_and(imagen, imagen, mask=mask)
+
+    # Dibujar el rectángulo en la imagen de salida
+    output = np.zeros_like(imagen)
+
+    # Crear una lista para almacenar las posiciones de los círculos detectados dentro del rectángulo
+    circle_positions = []
+
+    param1 = 50
+    param2 = 9
+    minRadius = 1
+    maxRadius = 14
+
+    # Dibujar los círculos detectados en la imagen de salida
+    circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 1, min(widthScreen, heightScreen) / 16, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
+
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+
+        # Almacenar las posiciones de los círculos detectados dentro del rectángulo
+        for (cx, cy, _) in circles:
+            if x <= cx <= x + w and y <= cy <= y + h:  # Verificar si el círculo está dentro del rectángulo
+                if cx <= centro + 15 and cy >= centro - 15 and cy > rect_y and cy < rect_y + rect_height:
+                    circle_positions.append((cx, cy))
+                    cv2.circle(output, (cx, cy), 5, (0, 255, 0), 2)  # Dibujar el círculo en la imagen de salida
+
+    
+        # Mantén el historial de longitud history_length
+        if circle_positions:
+            circle_positions_history.append(circle_positions)
+
+        history_length = 20
+        if len(circle_positions_history) > history_length:
+            circle_positions_history.pop(0)
+
+    # Combinar la imagen de la flauta con los círculos únicos detectados dentro del rectángulo
+    resultado = cv2.bitwise_or(flute_only, output)
+
+    return resultado'''
+
